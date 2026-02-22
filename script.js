cc2ff77e369c5d6e79621f0217ce9573300b5d59
SadeepaPK <sadeepatharaka02@gmail.com>
2026-02-22 20:39:02 +0530

    I updated the Index file to home page file

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function scrollToContact() {
-  document.getElementById("contact").scrollIntoView({
-    behavior: "smooth",
+const elements = document.querySelectorAll(".animate");
+
+window.addEventListener("scroll", () => {
+  elements.forEach((el) => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+    if (position < screenHeight - 100) {
+      el.classList.add("show");
+    }
   });
-}
+});
